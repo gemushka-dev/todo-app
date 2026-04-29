8031655fe19c209026aec6af9c8453175aac07f1
unknown <iskandar.django.work@gmail.com>
2026-04-29 13:55:23 +0500

    feat(router): setup basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+
 export const App = () => {
-  return <h1>Init Project</h1>;
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path="/" element={<h1>Main page</h1>} />
+        <Route path="/login" element={<h1>Login page</h1>} />
+        <Route path="/register" element={<h1>Register Page</h1>} />
+      </Routes>
+    </>
+  );
 };
