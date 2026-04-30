964bf2496e56d23660eddb7290374ed8bdb1101a
unknown <iskandar.django.work@gmail.com>
2026-04-30 10:35:29 +0500

    feat(identity): fetch user credentials

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,29 @@ import { LayOut } from "./components/Layout";
 import { HomePage } from "./pages/Homepage/Homepage";
 import "./style/main.css";
 import "./style/layout.css";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+  const [sharedData, setSharedData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3500/me", {
+        credentials: "include",
+      });
+      if (!response.ok) {
+        console.log("Error with cookies");
+      } else {
+        const data = await response.json();
+        setSharedData(data);
+        console.log(data);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <Routes>
-        <Route path="/" element={<LayOut />}>
+        <Route path="/" element={<LayOut data={sharedData} />}>
           <Route index path="/" element={<HomePage />} />
           <Route path="/login" element={<h1>Login page</h1>} />
           <Route path="/register" element={<h1>Register Page</h1>} />
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,23 @@
-import { NavData } from "../data/NavData";
+import { NavData, NavDataAuth } from "../data/NavData";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar = ({ data }: { data: object | null }) => {
   return (
     <>
       <nav className="nav">
         <h2>Logo</h2>
         <ul className="nav__list">
-          {NavData.map((el) => (
-            <li key={el.href} className="list__item">
-              <Link to={el.href}>{el.label}</Link>
-            </li>
-          ))}
+          {data
+            ? NavDataAuth.map((el) => (
+                <li key={el.href} className="list__item">
+                  <Link to={el.href}>{el.label}</Link>
+                </li>
+              ))
+            : NavData.map((el) => (
+                <li key={el.href} className="list__item">
+                  <Link to={el.href}>{el.label}</Link>
+                </li>
+              ))}
         </ul>
       </nav>
     </>
